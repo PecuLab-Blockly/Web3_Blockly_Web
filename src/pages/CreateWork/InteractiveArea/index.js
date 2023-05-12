544202ba550bc8275fa35db4cb6fed577c946c77
Memory-HuiYi <125955622+Memory-HuiYi@users.noreply.github.com>
2023-05-12 16:10:06 +0800

    blockly顯示JS跟結果

--- src/pages/CreateWork/InteractiveArea/index.js
+++ src/pages/CreateWork/InteractiveArea/index.js
@@ -7,19 +7,23 @@ const InteractiveArea = ({ codeResult }) => {
 
     useEffect(() => {
         var oldAlert = window.alert;
-        window.alert = function(message) {
+        window.alert = function (message) {
             setResult(message);
         };
 
         eval(codeResult);
 
-        window.alert = oldAlert; // 恢复原始的 window.alert 函数
+        window.alert = oldAlert; // 恢復原始的 window.alert 函数
     }, [codeResult]);
+
+    //增加可讀性
+    const formattedCodeResult = codeResult.split(';').map(line => line.trim()).join(';\n');
 
     return (
         <div className='game-zone'>
             <div>JavaScript:</div>
             <div>{codeResult}</div>
+            <pre>{formattedCodeResult}</pre>
             <div>執行結果:</div>
             <div>{result}</div>
         </div>
@@ -28,5 +32,4 @@ const InteractiveArea = ({ codeResult }) => {
 
 
 export default InteractiveArea;
-
 
