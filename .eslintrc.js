d81efba3b123173348d69ef81cea872ed0987c81
kelly <>
2023-06-14 15:07:35 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "eslint:recommended"],
+  extends: ['plugin:react/recommended', 'eslint:recommended'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/prop-types": 0,
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    indent: ["error", 2, { SwitchCase: 1 }],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    "jsx-quotes": ["error", "prefer-single"],
-    semi: ["error", "never"],
-    "semi-spacing": ["error"],
-    "comma-dangle": ["error", "never"],
-    "comma-spacing": "error",
-    "key-spacing": "error",
-    "arrow-spacing": "error",
-    "space-infix-ops": "error",
-    "brace-style": ["error", "1tbs", { allowSingleLine: true }],
-    camelcase: "error",
-    "new-cap": "error",
-    "space-before-blocks": "error",
-    "space-before-function-paren": [
-      "error",
+    'react/prop-types': 0,
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['off', 'windows'],
+    quotes: ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    semi: ['error', 'never'],
+    'semi-spacing': ['error'],
+    'comma-dangle': ['error', 'never'],
+    'comma-spacing': 'error',
+    'key-spacing': 'error',
+    'arrow-spacing': 'error',
+    'space-infix-ops': 'error',
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    camelcase: 'error',
+    'new-cap': 'error',
+    'space-before-blocks': 'error',
+    'space-before-function-paren': [
+      'error',
       {
-        anonymous: "never",
-        named: "never",
-        asyncArrow: "always",
-      },
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
     ],
-    "no-unused-vars": "off",
-    "no-unreachable": "error",
-    "computed-property-spacing": ["error", "never"],
-    curly: ["error", "all"],
-    "no-unneeded-ternary": "error",
-  },
-};
+    'no-unused-vars': 'off',
+    'no-unreachable': 'error',
+    'computed-property-spacing': ['error', 'never'],
+    curly: ['error', 'all'],
+    'no-unneeded-ternary': 'error'
+  }
+}
